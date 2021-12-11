0d707249f6d70082a432d082a54d0e45a29680a1
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2021-12-11 13:14:53 +0100

    rest of day7

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,10 +1,39 @@
-fn calculate(positions: &str) -> i32 {
-    let positions: Vec<i32> = positions.split(',').filter_map(|p| p.trim().parse::<i32>().ok()).collect();
-    calculate_distance(&positions, statistical::median(&positions))
+fn calculate_part1(positions: &str) -> i32 {
+    let positions: Vec<i32> = positions
+        .split(',')
+        .filter_map(|p| p.trim().parse::<i32>().ok())
+        .collect();
+
+    calculate_distance_part1(&positions, statistical::median(&positions))
 }
 
-fn calculate_distance(positions: &[i32], target: i32) -> i32 {
+fn calculate_distance_part1(positions: &[i32], target: i32) -> i32 {
     positions.iter().map(|&pos| (pos - target).abs()).sum()
+}
+
+fn calculate_part2(positions: &str) -> i32 {
+    let positions: Vec<i32> = positions
+        .split(',')
+        .filter_map(|p| p.trim().parse::<i32>().ok())
+        .collect();
+
+    let &from = positions.iter().min().unwrap();
+    let &to = positions.iter().max().unwrap();
+
+    (from..=to)
+        .map(|potential_target| calculate_distance_part2(&positions, potential_target))
+        .min()
+        .unwrap_or(0)
+}
+
+fn calculate_distance_part2(positions: &[i32], target: i32) -> i32 {
+    positions
+        .iter()
+        .map(|&pos| {
+            let steps = (pos - target).abs();
+            (steps * (steps + 1)) / 2 // Gauss formula for 1 + 2 + 3 + ... steps
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -12,15 +41,35 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn test_calculate_distance() {
-        let distance = calculate("16,1,2,0,4,2,7,1,2,14\n");
+    fn test_calculate_1() {
+        let distance = calculate_part1("16,1,2,0,4,2,7,1,2,14\n");
         assert_eq!(distance, 37)
     }
 
     #[test]
-    fn test_real_input() {
+    fn test_real_input_1() {
         let input = include_str!("../input.txt");
-        let distance = calculate(input);
+        let distance = calculate_part1(input);
         assert_eq!(distance, 336120)
+    }
+
+    #[test]
+    fn test_calculate_distance_part2() {
+        let positions = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+        assert_eq!(calculate_distance_part2(&positions, 5), 168);
+        assert_eq!(calculate_distance_part2(&positions, 2), 206);
+    }
+
+    #[test]
+    fn test_calculate_part2() {
+        let positions = "16, 1, 2, 0, 4, 2, 7, 1, 2, 14\n";
+        assert_eq!(calculate_part2(&positions), 168);
+    }
+
+    #[test]
+    fn test_real_input_2() {
+        let input = include_str!("../input.txt");
+        let distance = calculate_part2(input);
+        assert_eq!(distance, 96864235)
     }
 }
