0fafd5f5c8141515f0e2149b2d4629d6dae12d42
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2021-12-17 18:18:32 +0100

    Part2 of day9

--- day9/src/domain.rs
+++ day9/src/domain.rs
@@ -1,14 +1,16 @@
 use std::cmp::Ordering;
 use std::ops::{Add, Index};
+use std::str::FromStr;
 
 pub struct HeightMap {
     value: Vec<Vec<Height>>,
 }
 
 impl HeightMap {
-    pub fn new<'a, T>(nums: T) -> Self where T: Iterator<Item=&'a Vec<i32>> + 'a {
+    pub fn new(nums: Vec<Vec<i32>>) -> Self {
         HeightMap {
             value: nums
+                .iter()
                 .enumerate()
                 .map(|(row_index, value)| {
                     value
@@ -22,6 +24,15 @@ impl HeightMap {
         }
     }
 
+    pub fn low_points(&self) -> impl Iterator<Item=Height> + '_ {
+        self.iter_positions()
+            .filter(|&pos| {
+                self.adjacent(pos).iter().all(|&ad| ad > self[pos])
+            })
+            .map(|pos| self[pos])
+    }
+
+
     fn size(&self) -> MapSize {
         MapSize {
             max_rows: self.value.len() as i32,
@@ -29,8 +40,9 @@ impl HeightMap {
         }
     }
 
-    pub fn adjacent_heights(&self, position: Position) -> Vec<Height> {
-        let MapSize { max_rows, max_columns } = self.size();
+
+    fn adjacent(&self, position: Position) -> Vec<Height> {
+        let map_size = self.size();
         [
             Position(-1, 0),
             Position(0, -1),
@@ -39,26 +51,58 @@ impl HeightMap {
         ]
             .iter()
             .map(|delta| *delta + position)
-            .filter(|Position(row, column)| {
-                *row >= 0 && *column >= 0 && *row < max_rows && *column < max_columns
-            })
+            .filter(|pos| map_size.contains(pos))
             .map(|pos| self[pos])
             .collect()
     }
 
-    pub fn iter_positions(&self) -> impl Iterator<Item=Position> {
+    fn iter_positions(&self) -> impl Iterator<Item=Position> {
         let MapSize { max_rows, max_columns } = self.size();
         (0..max_rows).flat_map(move |row| (0..max_columns).map(move |column| Position(row, column)))
+    }
+
+    pub fn flood(&self, position: Position) -> Vec<Height> {
+        let mut result = Vec::new();
+
+        let mut points_to_check = vec![self[position]];
+
+        while !points_to_check.is_empty() {
+            let current = points_to_check.pop().unwrap();
+            let Height(position, height) = current;
+            if height < 9 && !result.contains(&current) {
+                result.push(current);
+                points_to_check.append(&mut self.adjacent(position))
+            }
+        }
+
+        return result
     }
 }
 
 impl Index<Position> for HeightMap {
     type Output = Height;
 
-    fn index(&self, Position(row, column): Position) -> &Self::Output {
-        &self.value[row as usize][column as usize]
+    fn index(&self, index: Position) -> &Self::Output {
+        let Position(row, column) = index;
+        return &self.value[row as usize][column as usize];
     }
 }
+
+impl FromStr for HeightMap {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let result: Vec<Vec<i32>> = input
+            .lines()
+            .map(|line| line
+                .split("")
+                .filter_map(|s| s.parse::<i32>().ok())
+                .collect())
+            .collect();
+        Ok(HeightMap::new(result))
+    }
+}
+
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct Position(pub i32, pub i32);
@@ -75,7 +119,7 @@ impl Add for Position {
 pub struct Height(pub Position, pub i32);
 
 impl PartialOrd for Height {
-    fn partial_cmp(&self, Height(pos, height): &Self) -> Option<Ordering> {
+    fn partial_cmp(&self, Height(_pos, height): &Self) -> Option<Ordering> {
         self.1.partial_cmp(height)
     }
 }
@@ -85,6 +129,13 @@ pub struct MapSize {
     max_rows: i32,
     max_columns: i32,
 }
+
+impl MapSize {
+    fn contains(&self, &Position(row, column): &Position) -> bool {
+        row >= 0 && column >= 0 && row < self.max_rows && column < self.max_columns
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -97,7 +148,8 @@ mod tests {
             vec![4, 5, 6],
             vec![7, 8, 9],
         ];
-        let map = HeightMap::new(val.iter());
+
+        let map = HeightMap::new(val);
         assert_eq!(map[Position(1, 1)], Height(Position(1, 1), 5))
     }
 
@@ -108,8 +160,8 @@ mod tests {
             vec![4, 5, 6],
             vec![7, 8, 9],
         ];
-        let map = HeightMap::new(val.iter());
-        let points = map.adjacent_heights(Position(1, 1));
+        let map = HeightMap::new(val);
+        let points = map.adjacent(Position(1, 1));
         assert_eq!(points, vec![
             Height(Position(0, 1), 2),
             Height(Position(1, 0), 4),
@@ -126,11 +178,38 @@ mod tests {
             vec![4, 5, 6],
             vec![7, 8, 9],
         ];
-        let map = HeightMap::new(val.iter());
-        let points = map.adjacent_heights(Position(2, 2));
+        let map = HeightMap::new(val);
+        let points = map.adjacent(Position(2, 2));
         assert_eq!(points, vec![
             Height(Position(1, 2), 6),
             Height(Position(2, 1), 8),
+        ])
+    }
+
+    #[test]
+    fn test_fill_flood() {
+        let map: HeightMap = "\
+            2199943210\n\
+            3987894921\n\
+            9856789892\n\
+            8767896789\n\
+            9899965678\n"
+            .parse()
+            .unwrap();
+
+        let result = map.flood(Position(0, 9));
+
+        assert_eq!(result, vec![
+            Height(Position(0,9), 0),
+            Height(Position(1,9), 1),
+            Height(Position(2,9), 2),
+            Height(Position(1,8), 2),
+            Height(Position(0,8), 1),
+
+            Height(Position(0,7), 2),
+            Height(Position(0,6), 3),
+            Height(Position(1,6), 4),
+            Height(Position(0,5), 4),
         ])
     }
 }
